7ff11f3e00d9fb5796abb5ee3485f2cccc511679
kpla001 <kpla001@fiu.edu>
2022-02-14 18:33:16 -0500

    (prod): complete Nest.JS tutorial part 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://kyle:shyHope88@nestjs-cluster0.71rnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
